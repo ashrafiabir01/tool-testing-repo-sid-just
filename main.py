49cc3b3a876e94f3688b61b5357d548f99927447
Ashrafi Abir <90349666+ashrafiabir01@users.noreply.github.com>
2022-08-29 03:18:23 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+tool testing repo sid just
+
+import os
+os.system('cls')
+data = 'AC29b30cb6927c9de11ff906e3ec63986d'
+data = '37dcdc36716bd28af12b828509f248fd'
+data3 = 'tool testing repo sid just'
